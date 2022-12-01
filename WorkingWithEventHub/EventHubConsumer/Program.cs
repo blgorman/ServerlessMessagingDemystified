eedfdf8aca196ba7e3372954aabce928f95fcb1a
Brian L. Gorman <blgorman@gmail.com>
2022-12-01 11:45:45 -0600

    updates to actually process the text to storage so that there is a better understanding of how this works with storage.

--- WorkingWithEventHub/EventHubConsumer/Program.cs
+++ WorkingWithEventHub/EventHubConsumer/Program.cs
@@ -24,6 +24,8 @@ namespace EventHubConsumer
         private static EventProcessorClient _processor;
         private static BlobContainerClient _storageClient;
 
+        private static Random r = new Random();
+
         public static async Task Main(string[] args)
         {
             BuildOptions();
@@ -36,10 +38,12 @@ namespace EventHubConsumer
 
             // Read from the default consumer group: $Default
             string consumerGroup = EventHubConsumerClient.DefaultConsumerGroupName;
-            _storageClient = new BlobContainerClient(_blobStorageConnectionString, _blobContainerName);
+            _storageClient = new BlobContainerClient(_blobStorageConnectionString
+                                                            , _blobContainerName);
 
             // Create an event processor client to process events in the event hub
-            _processor = new EventProcessorClient(_storageClient, consumerGroup, _eventHubConnectionString, _eventHubName);
+            _processor = new EventProcessorClient(_storageClient, consumerGroup
+                                    , _eventHubConnectionString, _eventHubName);
 
             // Register handlers for processing events and handling errors
             _processor.ProcessEventAsync += ProcessEventHandler;
@@ -57,10 +61,23 @@ namespace EventHubConsumer
 
         private static async Task ProcessEventHandler(ProcessEventArgs eventArgs)
         {
+            var eventArgsMessageData = eventArgs.Data.Body.ToArray();
+            var eventArgsMessageString = Encoding.UTF8.GetString(eventArgsMessageData);
+            
             // Write the body of the event to the console window
-            Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+            Console.WriteLine($"\tReceived event: {eventArgsMessageString}");
 
-            // Update checkpoint in the blob storage so that the app receives only new events the next time it's run
+            // Save the data to a text file at Storage:
+            var fileName = $"{DateTime.Now.ToString("yyyy.MM.dd_")}{r.Next(1000, 1999)}.txt";
+            var blobClient = _storageClient.GetBlobClient(fileName);
+
+            using (var ms = new MemoryStream(eventArgsMessageData))
+            {
+                await blobClient.UploadAsync(ms, true);
+            }
+
+            // Update checkpoint in the consumer group so that the app processes
+            // only new events the next time it's run
             await eventArgs.UpdateCheckpointAsync(eventArgs.CancellationToken);
         }
 
