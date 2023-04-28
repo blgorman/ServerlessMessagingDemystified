8008c61cbf4d2e1446406dc2956f40f75f8d34b0
Brian L. Gorman <blgorman@gmail.com>
2023-04-28 15:46:06 -0500

    updated the code for the service bus queue to modernize single message read and make the program more robust

--- WorkingWithServiceBus/ServiceBusQueue/ServiceBusQueueConsumer/Program.cs
+++ WorkingWithServiceBus/ServiceBusQueue/ServiceBusQueueConsumer/Program.cs
@@ -1,5 +1,4 @@
 ﻿using Azure.Messaging.ServiceBus;
-using Microsoft.Azure.ServiceBus;
 using Microsoft.Extensions.Configuration;
 using MySolutionObjectModels;
 using Newtonsoft.Json;
@@ -11,18 +10,16 @@ namespace ServiceBusQueueConsumer
     {
         private static IConfigurationRoot _configuration;
         private static string _sbConnectionString;
-        private static string _sbOnlyOneConnectionString;
         private static string _sbQueueName;
-
+        private static int MAX_WAIT_TIME_SECONDS = 20;
 
         public static async Task Main(string[] args)
         {
             BuildOptions();
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Welcome to the Service Bus Queue Processing Demos");
 
             //add the service bus connection and clients
             _sbConnectionString = _configuration["ServiceBus:ReadOnlyConnectionString"];
-            _sbOnlyOneConnectionString = _configuration["ServiceBus:ReadOnlyConnectionStringWithoutEntityPath"];
             _sbQueueName = _configuration["ServiceBus:QueueName"];
 
             if (string.IsNullOrEmpty(_sbConnectionString) || string.IsNullOrEmpty(_sbQueueName))
@@ -30,71 +27,81 @@ namespace ServiceBusQueueConsumer
                 Console.WriteLine("Ensure connection string and queue name are set correctly");
                 return;
             }
-            
-            ProcessOnlyOneMessage();
 
-            await TypicalQueueProcessing();
+            Console.WriteLine("Would you like to process a single message? (y/n)");
+            bool shouldContinue = Console.ReadLine().ToLower().StartsWith("y");
+            while (shouldContinue)
+            {
+                Console.WriteLine("Processing next message!");
+                await ProcessOnlyOneMessageAsync();
+                Console.WriteLine("Would you like to process another single message? (y/n)");
+                shouldContinue = Console.ReadLine().ToLower().StartsWith("y");
+            }
+
+            Console.WriteLine("Polling all remaining messages...hit any key to stop polling");
+            await PollingQueueProcessingAsync();
+
+            Console.WriteLine("Press any key to end the program");
+            Console.ReadKey();
         }
 
-        private static async void ProcessOnlyOneMessage()
+        private static async Task ProcessOnlyOneMessageAsync()
         {
-            //NOTE: This is now deprecated, as noted in the nuget package manager
-            //      but it still works at this time.
-            //https://www.michalbialecki.com/en/2018/05/21/receiving-only-one-message-from-azure-service-bus/
-            var queueClient = new QueueClient(_sbOnlyOneConnectionString, _sbQueueName);
+            var client = new ServiceBusClient(_sbConnectionString);
+            var options = new ServiceBusReceiverOptions();
+            options.PrefetchCount = 1;
 
-            var messageBody = string.Empty;
-            var movie = new Movie();
+            //at-most-once-delivery
+            options.ReceiveMode = ServiceBusReceiveMode.ReceiveAndDelete;
 
+            //create a receiver that we can use to process the messages one at a time
+            var receiver = client.CreateReceiver(_sbQueueName, options);
+
+            
             try
             {
-                queueClient.RegisterMessageHandler(
-                async (message, token) =>
+                //max wait time is MAX_WAIT_TIME_SECONDS, if no messages are received by then, we will exit
+                var waitTime = TimeSpan.FromSeconds(MAX_WAIT_TIME_SECONDS);
+                var msg = await receiver.ReceiveMessageAsync(waitTime);
+
+                if (msg is null || msg?.Body is null)
                 {
-                    messageBody = Encoding.UTF8.GetString(message.Body);
-                    Console.WriteLine($"Received: {messageBody}");
+                    Console.WriteLine("No messages in the queue and wait time has elapsed");
+                    return;
+                }
 
-                    movie = JsonConvert.DeserializeObject<Movie>(messageBody);
-                    Console.WriteLine($"Movie converted: {movie.Title} | {movie.MPAARating}");
+                string body = msg.Body.ToString();
+                Console.WriteLine($"Received: {body}");
 
-                    await queueClient.CompleteAsync(message.SystemProperties.LockToken);
-                    await queueClient.CloseAsync();
-                },
-                new MessageHandlerOptions(async args => Console.WriteLine(args.Exception))
-                { MaxConcurrentCalls = 1, AutoComplete = false });
+                var movie = JsonConvert.DeserializeObject<Movie>(body);
+                await WriteMessageToConsole(movie);
 
-                Console.WriteLine($"Message: {messageBody}");
+                //no need to complete because we are using ReceiveAndDelete!
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                Console.WriteLine($"Error: {ex.Message}");
             }
-
-            var autoTimeOut = DateTime.Now.AddSeconds(30);
-            while (string.IsNullOrWhiteSpace(movie?.Title) && DateTime.Now < autoTimeOut)
+            finally
             {
-                Thread.Sleep(1000);
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await receiver.DisposeAsync();
+                await client.DisposeAsync();
             }
-
-            if (string.IsNullOrWhiteSpace(movie?.Title))
-            {
-                Console.WriteLine(new Exception("No data returned from the queue for processing"));
-                return;
-            }
-
-            await WriteMessageToConsole(movie);
-            
         }
 
-        private static async Task TypicalQueueProcessing()
+        private static async Task PollingQueueProcessingAsync()
         {
             var client = new ServiceBusClient(_sbConnectionString);
-            var processor = client.CreateProcessor(_sbQueueName, new ServiceBusProcessorOptions());
+            //create a processor that we can use to process the messages until we decide to stop
+            var processor = client.CreateProcessor(_sbQueueName, new ServiceBusProcessorOptions()); 
 
             try
             {
                 // add handler to process messages
                 processor.ProcessMessageAsync += MessageHandler;
+                
 
                 // add handler to process any errors
                 processor.ProcessErrorAsync += ErrorHandler;
@@ -102,7 +109,6 @@ namespace ServiceBusQueueConsumer
                 // start processing 
                 await processor.StartProcessingAsync();
 
-                Console.WriteLine("Wait for a minute and then press any key to end the processing");
                 Console.ReadKey();
 
                 // stop processing 
@@ -128,6 +134,7 @@ namespace ServiceBusQueueConsumer
             Console.WriteLine($"Movie Review found: {movie.Title} was released in {movie.ReleaseYear} and is rated {movie.MPAARating}");
         }
 
+
         // handle received messages
         private static async Task MessageHandler(ProcessMessageEventArgs args)
         {
@@ -139,6 +146,8 @@ namespace ServiceBusQueueConsumer
 
             // complete the message. message is deleted from the queue. 
             await args.CompleteMessageAsync(args.Message);
+
+            System.Threading.Thread.Sleep(2000);
         }
 
         // handle any errors when receiving messages
